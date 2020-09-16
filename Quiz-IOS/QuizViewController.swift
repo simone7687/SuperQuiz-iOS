b909bee1bd93c7969d4d922e1ba5f76980a07a7a
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-09-16 09:39:32 -0700

    QuizViewController

---
+++ Quiz-IOS/QuizViewController.swift
@@ -0,0 +1,22 @@
+//
+//  QuizViewController.swift
+//  Quiz-IOS
+//
+//  Created by Simone on 16/09/2020.
+//  Copyright © 2020 Simone Massaro. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var answer1: UIButton!
+    @IBOutlet weak var answer2: UIButton!
+    @IBOutlet weak var answer3: UIButton!
+    @IBOutlet weak var answer4: UIButton!
+    @IBOutlet weak var continueButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
