332e1b52bb6e77de1659f58236fd6aafece94e96
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-09-16 18:00:30 +0200

    Quiz

---
+++ Quiz-IOS/Quiz.swift
@@ -0,0 +1,29 @@
+//
+//  Quiz.swift
+//  Quiz-IOS
+//
+//  Created by Simone on 16/09/2020.
+//  Copyright © 2020 Simone Massaro. All rights reserved.
+//
+
+import Foundation
+
+enum QuizType {
+    case Normat
+    case Multiple
+    case Open
+}
+
+struct Quiz {
+    var type : QuizType
+    var question : String
+    var answers : [String]
+    var correctAnswers : [Int]
+    
+    init(type : Quiz, question : String, answers : [String], correctAnswers : [Int]) {
+        self.type = type
+        self.question = question
+        self.answers = answers
+        self.correctAnswers = correctAnswers
+    }
+}
